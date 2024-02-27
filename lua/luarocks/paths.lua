50b9f43b4b28072f3f5f3654a27434679cd455f9
Vhyrro <vhyrro@gmail.com>
2024-02-27 15:07:50 +0100

    revert: return back to old code as `vim.fs.joinpath` is nightly-only

--- lua/luarocks/paths.lua
+++ lua/luarocks/paths.lua
@@ -9,6 +9,10 @@ local function get_path_separator()
 	return "/"
 end
 
+local function combine_paths(...)
+	return table.concat({ ... }, get_path_separator())
+end
+
 local function get_plugin_path()
 	local str = debug.getinfo(2, "S").source:sub(2)
 	if is_win() then
@@ -18,20 +22,20 @@ local function get_plugin_path()
 end
 
 local plugin_path = get_plugin_path()
-local rocks_path = vim.fs.joinpath(plugin_path, ".rocks")
+local rocks_path = combine_paths(plugin_path, ".rocks")
 
 return {
 	plugin = plugin_path,
 	rocks = rocks_path,
-	bin = vim.fs.joinpath(rocks_path, "bin"),
-	luarocks = vim.fs.joinpath(rocks_path, "bin", "luarocks"),
-	pip = vim.fs.joinpath(rocks_path, "bin", "pip3"),
-	hererocks = vim.fs.joinpath(rocks_path, "bin", "hererocks"),
-	build_cache = vim.fs.joinpath(rocks_path, "builds"),
+	bin = combine_paths(rocks_path, "bin"),
+	luarocks = combine_paths(rocks_path, "bin", "luarocks"),
+	pip = combine_paths(rocks_path, "bin", "pip3"),
+	hererocks = combine_paths(rocks_path, "bin", "hererocks"),
+	build_cache = combine_paths(rocks_path, "builds"),
 	share = {
-		vim.fs.joinpath(rocks_path, "share", "lua", "5.1", "?.lua"),
-		vim.fs.joinpath(rocks_path, "share", "lua", "5.1", "?", "init.lua"),
+		combine_paths(rocks_path, "share", "lua", "5.1", "?.lua"),
+		combine_paths(rocks_path, "share", "lua", "5.1", "?", "init.lua"),
 	},
-	lib = vim.fs.joinpath(rocks_path, "lib", "lua", "5.1", "?.so"),
-	rockspec = vim.fs.joinpath(rocks_path, "neovim-rocks-user-rockspec-0.0-0.rockspec"),
+	lib = combine_paths(rocks_path, "lib", "lua", "5.1", "?.so"),
+	rockspec = combine_paths(rocks_path, "neovim-rocks-user-rockspec-0.0-0.rockspec"),
 }
