6909310ca96337d83f7612492d8d24c2ece9845d
Vhyrro <vhyrro@gmail.com>
2024-02-27 15:26:07 +0100

    fix: remove `vim.schedule`

--- build.lua
+++ build.lua
@@ -2,8 +2,6 @@ package.path = "./lua/?.lua;" .. package.path
 
 local build = require("luarocks.build")
 
-vim.schedule(function()
-	if not build.is_prepared() then
-		build.build()
-	end
-end)
+if not build.is_prepared() then
+    build.build()
+end
