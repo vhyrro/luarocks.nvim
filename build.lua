cfe15a6c24a1c599f655a83cd5a1aa9535a93a71
Vhyrro <vhyrro@gmail.com>
2024-02-28 20:20:53 +0100

    revert: add vim.schedule back to build.lua

--- build.lua
+++ build.lua
@@ -2,6 +2,8 @@ package.path = "./lua/?.lua;" .. package.path
 
 local build = require("luarocks.build")
 
-if not build.is_prepared() then
-    build.build()
-end
+vim.schedule(function()
+	if not build.is_prepared() then
+		build.build()
+	end
+end)
