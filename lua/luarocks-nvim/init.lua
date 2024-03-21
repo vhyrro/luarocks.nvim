b7958dc18ec4d5196325599e5023b02e80dec4bb
Vhyrro <vhyrro@gmail.com>
2024-03-21 18:57:47 +0100

    feat: add luarocks loader for many versions of the same dependency

--- lua/luarocks-nvim/init.lua
+++ lua/luarocks-nvim/init.lua
@@ -16,6 +16,12 @@ return {
 
 		-- Check that the system is ready to install rocks
 		if build.is_prepared() then
+            -- Invoke the luarocks loader. This allows many versions of the same
+            -- dependency to exist within the Lua environment.
+            --
+            -- See https://github.com/luarocks/luarocks/wiki/Using-LuaRocks#multiple-versions-using-the-luarocks-package-loader for details.
+            require("luarocks.loader")
+
 			-- We have requested rocks so ensure they are installed
 			rocks.ensure(opts.rocks)
 		else
