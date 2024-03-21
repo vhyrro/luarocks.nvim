816597b74d442ec8ffe15c6f943d3daab3980a1f
pysan3 <pysan3@gmail.com>
2024-03-22 01:43:50 +0900

    fix(build): simplify build code and better error message

--- lua/luarocks/build.lua
+++ lua/luarocks/build.lua
@@ -10,7 +10,7 @@ local function is_prepared()
 end
 
 local function remove_shell_color(s)
-    return tostring(s):gsub("\x1B%[[0-9;]+m", "")
+	return vim.trim(tostring(s):gsub("\x1B%[[0-9;]+m", ""))
 end
 
 --- Checks if a system tool is available.
@@ -26,6 +26,30 @@ math.randomseed(os.time())
 local tempdir =
 	utils.combine_paths(vim.fn.stdpath("run") --[[@as string]], ("luarocks-%X"):format(math.random(256 ^ 7)))
 
+---Run job and return its exit_code, stdout, stderr
+---@param args string[]
+---@return integer exit_code
+---@return string stdout
+---@return string stderr
+local function run_job(args)
+	local stdout = {}
+	local stderr = {}
+	local job = vim.fn.jobstart(args, {
+		cwd = tempdir,
+		stdout_buffered = true,
+		stderr_buffered = true,
+		on_stdout = function(_, data)
+			vim.list_extend(stdout, data or {})
+		end,
+		on_stderr = function(_, data)
+			vim.list_extend(stderr, data or {})
+		end,
+	})
+
+	return vim.fn.jobwait({ job })[1],
+		table.concat(vim.tbl_map(remove_shell_color, stdout), "\n"),
+		table.concat(vim.tbl_map(remove_shell_color, stderr), "\n")
+end
 
 local rocks_after_build = nil
 local luarocks_args = nil
@@ -62,93 +86,64 @@ local steps = {
 		end,
 	},
 	{
-		description = "Performing a local installation",
+		description = "Performing luarocks install.bat if Windows systems",
 		task = function()
-            local error_output
-
-			if is_windows then
-                local cmd = {
-					"cmd.exe",
-					"/c",
-					"install.bat",
-					"/P",
-					paths.rocks,
-					"/LV",
-					"5.1",
-					"/FORCECONFIG",
-					"/NOADMIN",
-					"/Q",
-				}
-
-                vim.list_extend(cmd, luarocks_args or {})
-
-				local job = vim.fn.jobstart(cmd, {
-					cwd = tempdir,
-                    stdout_buffered = true,
-					on_stdout = function(_, data)
-						local concatenated = table.concat(data, "\n")
-
-                        if vim.trim(concatenated):len() == 0 then
-                            return
-                        end
-
-                        error_output = remove_shell_color(concatenated)
-					end,
-				})
-
-				local error_code = vim.fn.jobwait({ job })[1]
-
-				assert(error_code == 0, string.format("Failed to install luarocks: %s", error_output))
-			else
-                local cmd = {
-					"sh",
-					"configure",
-					"--prefix=" .. paths.rocks,
-					"--lua-version=5.1",
-					"--force-config",
-				}
-
-                vim.list_extend(cmd, luarocks_args or {})
-
-				local job = vim.fn.jobstart(cmd, {
-					cwd = tempdir,
-                    stdout_buffered = true,
-					on_stdout = function(_, data)
-						local concatenated = table.concat(data, "\n")
-
-                        if vim.trim(concatenated):len() == 0 then
-                            return
-                        end
-
-                        error_output = remove_shell_color(concatenated)
-					end,
-				})
-
-				local error_code = vim.fn.jobwait({ job })[1]
-
-				assert(error_code == 0, string.format("Failed to install luarocks: %s", error_output))
-
-				job = vim.fn.jobstart({
-					"make",
-					"install",
-				}, {
-					cwd = tempdir,
-                    stdout_buffered = true,
-					on_stdout = function(_, data)
-						local concatenated = table.concat(data, "\n")
-
-                        if vim.trim(concatenated):len() == 0 then
-                            return
-                        end
-
-                        error_output = remove_shell_color(concatenated)
-					end,
-				})
-
-				error_code = vim.fn.jobwait({ job })[1]
-
-				assert(error_code == 0, string.format("Failed to install luarocks: %s", error_output))
+			if not is_windows then
+				return
 			end
+			local cmd = {
+				"cmd.exe",
+				"/c",
+				"install.bat",
+				"/P",
+				paths.rocks,
+				"/LV",
+				"5.1",
+				"/FORCECONFIG",
+				"/NOADMIN",
+				"/Q",
+			}
+			vim.list_extend(cmd, luarocks_args or {})
+			local error_code, stdout, stderr = run_job(cmd)
+			assert(error_code == 0, string.format("Failed to install luarocks: %s\n%s", stdout, stderr))
+		end,
+	},
+	{
+		description = "Performing luarocks `./configure` if Unix systems",
+		task = function()
+			if is_windows then
+				return
+			end
+			local cmd = {
+				"sh",
+				"configure",
+				"--prefix=" .. paths.rocks,
+				"--lua-version=5.1",
+				"--force-config",
+			}
+			vim.list_extend(cmd, luarocks_args or {})
+			local error_code, stdout, stderr = run_job(cmd)
+			assert(error_code == 0, string.format("Failed to install luarocks: %s\n%s", stdout, stderr))
+		end,
+	},
+	{
+		description = "Performing luarocks `make` if Unix systems",
+		task = function()
+			if is_windows then
+				return
+			end
+			local error_code, stdout, stderr = run_job({ "make" })
+			assert(error_code == 0, string.format("Failed to install luarocks: %s\n%s", stdout, stderr))
+		end,
+	},
+	{
+		description = "Performing luarocks `make install` if Unix systems",
+		task = function()
+			if is_windows then
+				return
+			end
+			local error_code, stdout, stderr = run_job({ "make", "install" })
+			assert(error_code == 0, string.format("Failed to install luarocks: %s\n%s", stdout, stderr))
 		end,
 	},
 }
@@ -179,6 +174,6 @@ return {
 	-- after the build
 	ensure_rocks_after_build = function(ensure_rocks, args)
 		rocks_after_build = ensure_rocks
-        luarocks_args = args
+		luarocks_args = args
 	end,
 }
