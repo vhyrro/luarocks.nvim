3f7fb78d660a5425ed00bc751f37ea6151517650
Vhyrro <vhyrro@gmail.com>
2024-03-22 19:10:49 +0100

    feat: add back the healthcheck

--- lua/luarocks-nvim/health.lua
+++ lua/luarocks-nvim/health.lua
@@ -4,14 +4,29 @@ return {
 	check = function()
 		vim.health.start("luarocks.nvim")
 		if build.is_prepared() then
-			vim.health.ok("luarocks system is prepared and ready to install luarocks")
+			vim.health.ok("luarocks is installed, system is prepared to install rocks!")
 		else
 			vim.health.info("luarocks system is not prepared")
-			-- if build.is_python_available() then
-			-- 	vim.health.ok("python3 is available on PATH, and rocks system is ready to be built")
-			-- else
-			-- 	vim.health.error("python3 is not available on PATH")
-			-- end
+
+            if vim.fn.executable("git") == 0 then
+                vim.health.ok("git is installed")
+            else
+                vim.health.error("git is not installed! Please install it at https://git-scm.com/downloads.")
+            end
+
+            if vim.fn.executable("lua") == 1 then
+                vim.health.ok("lua is installed")
+            else
+                vim.health.error("lua is not installed! If you're on unix, please install it using your package manager. For windows use https://github.com/rjpcomputing/luaforwindows.")
+            end
+
+            if not require("luarocks-nvim.utils").is_win() then
+                if vim.fn.executable("make") == 1 then
+                    vim.health.ok("make is installed")
+                else
+                    vim.health.error("make is not installed!")
+                end
+            end
 		end
 	end,
 }
